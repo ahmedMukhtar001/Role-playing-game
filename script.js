ba53b7c54f3d0d6852dd2b39d54b02ec4077a315
ahmedMukhtar001 <kanzpanther@gmail.com>
2023-12-27 09:40:48 +0300

    third object to the locations

--- script.js
+++ script.js
@@ -23,13 +23,21 @@ const locations = [
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
         "button functions": [goStore,goCave,fightDragon],
         text: "You are in the town square. You see a sign that says \"Store\"."
-    }
+    },
     {
         name: "store",
         "button text": ["Buy 10 health (10 gold)","Buy weapon (30 gold)","Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
-      }
+    },
+
+    {
+        name:"cave",
+        "button text": ["Fight slime","Fight fanged beast","Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text:"You enter the cave. You see some monsters."
+    }  
+
 ];
 // initialize buttons
 button1.onclick = goStore;
@@ -69,7 +77,7 @@ function goCave() {
   function buyWeapon() {
   
   }
-  
+
   function fightSlime() {
 
   }
