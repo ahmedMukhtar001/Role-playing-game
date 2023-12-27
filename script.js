9af41c1d2fab8bffc0feb9ea1200c44ed6fdfbb8
ahmedMukhtar001 <kanzpanther@gmail.com>
2023-12-27 09:43:22 +0300

    update

--- script.js
+++ script.js
@@ -63,8 +63,8 @@ function goTown() {
 }
 
 function goCave() {
-    console.log("Going to cave.");
-  }
+    update(locations[2]);
+}
   
   function fightDragon() {
     console.log("Fighting dragon.");
