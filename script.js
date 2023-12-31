417193d07d2aadc5f5d064988ffcfac96b240af7
ahmedMukhtar001 <kanzpanther@gmail.com>
2023-12-31 16:03:47 +0300

    accessing an object within the weapons

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function goCave() {
       gold -= 30;
       currentWeapon++;
       goldText.innerText = gold;
-      let newWeapon = weapons;
+      let newWeapon = weapons[currentWeapon];
       text.innerText = "You now have a new weapon.";
     }
   }
