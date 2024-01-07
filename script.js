588e9ac3195ccfb8e0bf6b5fe839d0a37a0fd113
ahmedMukhtar001 <kanzpanther@gmail.com>
2024-01-07 15:27:45 +0300

    currentWeapon

--- script.js
+++ script.js
@@ -99,17 +99,19 @@ function goCave() {
     }
   }
   
-  function buyWeapon() {
-    if (gold >= 30) {
-      gold -= 30;
-      currentWeapon++;
-      goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon].name;
-      text.innerText = "You now have a " + newWeapon + ".";
-      inventory.push(newWeapon);
-      text.innerText+=' In your inventory you have: ' + inventory;
-    }else {
-      text.innerText = "You do not have enough gold to buy a weapon."
+  if (currentWeapon < 3) {
+    function buyWeapon() {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText+=' In your inventory you have: ' + inventory;
+      }else {
+        text.innerText = "You do not have enough gold to buy a weapon."
+      }
     }
   }
   function fightSlime() {
