001e439df91139b7b0c09355a71b18e7f15d70eb
ahmedMukhtar001 <kanzpanther@gmail.com>
2024-01-12 21:16:47 +0300

    sellWeapon

--- script.js
+++ script.js
@@ -125,6 +125,8 @@ function goCave() {
       let currentWeapon = inventory.shift();
       text.innerText = "You sold a " + currentWeapon + "."
       text.innerText += " In your inventory you have: " + inventory
+    }else {
+      text.innerText = "Don't sell your only weapon!"
     }
   }
   function fightSlime() {
